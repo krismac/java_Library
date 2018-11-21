49cf11777c52cc0d51423bf80f9222004dea8e85
krismac <email@krismac.com>
2018-11-21 09:12:33 +0000

    struggling with adding from one and removing from another

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private ArrayList<Book> collection;
+    private int maxCollection;
+
+    public Borrower() {
+        this.collection = new ArrayList<>();
+        this.maxCollection = 3;
+    }
+
+    public void addBookToCollection(Book book) {
+        if (this.collection.size() < this.maxCollection) {
+            this.collection.add(book);
+        }
+        return;
+    }
+
+    public int getCollectionCount() {
+        return this.collection.size();
+    }
+
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,22 +1,25 @@
-//import org.junit.Test;
-//
-//public class BorrowerTest {
-//
-//    //Add a third class which interacts with the other two.
-//    //E.g. you could add a Borrower with a method that takes a Book and moves to the Borrower's collection.
-//
-//    //instance variables
-//    Book book;
-//    Borrower borrower;
-//    Library library
-//
-//    @Test
-//    public void removeBookToCollection(){
-//        library.addBook(book);
-//        library.removeBooktoCollection(book);
-//        assertEquals(0, Collection.Count());
-//
-//
-//}
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BorrowerTest {
+
+    //Add a third class which interacts with the other two.
+    //E.g. you could add a Borrower with a method that takes a Book and moves to the Borrower's collection.
+
+    //instance variables
+    Book book;
+    Borrower borrower;
+    Library library;
+
+
+    @Test
+    public void removeBookToCollection() {
+        library.addBook(book);
+        assertEquals(1, borrower.addBookToCollection());
+        assertEquals(0, borrower.getCollectionCount());
+    }
+
+}
 
 
